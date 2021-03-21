23d8a031b409c3494b1b4623d630d90a57845184
Yonchi03 <joandenchev@gmail.com>
2021-03-21 04:16:52 +0200

    Version 1.0

--- Project/Run2.java
+++ Project/Run2.java
@@ -15,3 +15,15 @@ public class Run2 {
         System.gc();
     }
 }
+
+class RandomNumbers {
+
+    public static float getRandomFloat(float min, float max) {
+        return (float) ((Math.random() * ((max - min) + 1)) + min);
+    }
+
+    public static int getRandomInteger(int min, int max) {
+        return (int) ((Math.random() * ((max - min) + 1)) + min);
+    }
+
+}

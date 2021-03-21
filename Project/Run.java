2c47bb4ba26f740b5ca35b077dcea4d0ab040cb9
Yonchi03 <joandenchev@gmail.com>
2021-03-21 02:49:40 +0200

    Swap Lists, added memory test

--- Project/Run.java
+++ Project/Run.java
@@ -7,36 +7,36 @@ public class Run {
     public static void main(String[] args) {
 
         ///////////////////////////////////////////////////////////////////////
-        long startTime1 = System.nanoTime();
-
-        //Creating house objects
-        for (int i = 0; i < 300000; i++){
-            House House1 = new House(250.6f, "Location 1", 3, "\"The owner's name\"", false);
-            House House2 = new House(410.2f, "Location 2", 5, "\"The owner's name\"", false);
-            House House3 = new House(350.5f, "Location 3", 3, "\"The owner's name\"", false);
-            House House4 = new House(380.0f, "Location 4", 4, "\"The owner's name\"", false);
-        }
-
-        for (House i : House.getHouseArrayList()) i.fancyPrint();
-        House.printMostSpaciousAL();
-
-        long endTime1 = System.nanoTime();
-
-
-        ///////////////////////////////////////////////////////////////////////
+        long before0 = Runtime.getRuntime().freeMemory();
         long startTime2 = System.nanoTime();
 
-        for (int i = 0; i < 300000; i++){
-            House House1 = new House(250.6f, "Location 1", 3, "\"The owner's name\"", true);
-            House House2 = new House(410.2f, "Location 2", 5, "\"The owner's name\"", true);
-            House House3 = new House(350.5f, "Location 3", 3, "\"The owner's name\"", true);
-            House House4 = new House(380.0f, "Location 4", 4, "\"The owner's name\"", true);
-        }
+        House House5 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", true);
+        House House6 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", true);
+        House House7 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", true);
+        House House8 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", true);
+
 
         for (House i : House.getHouseLinkedList()) i.fancyPrint();
         House.printMostSpaciousLL();
 
         long endTime2 = System.nanoTime();
+        long used0 = before0 - Runtime.getRuntime().freeMemory();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        long before = Runtime.getRuntime().freeMemory();
+        long startTime1 = System.nanoTime();
+
+        House House1 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", false);
+        House House2 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", false);
+        House House3 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", false);
+        House House4 = new House(RandomNumbers.getRandomDouble(200, 700), "Location", RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", false);
+
+        for (House i : House.getHouseArrayList()) i.fancyPrint();
+        House.printMostSpaciousAL();
+
+        long endTime1 = System.nanoTime();
+        long used = before - Runtime.getRuntime().freeMemory();
 
 
         ///////////////////////////////////////////////////////////////////////
@@ -44,10 +44,16 @@ public class Run {
         long duration2 = (endTime2 - startTime2);
 
         System.out.printf("""
-                ArrayList  Duration: %s
-                LinkedList Duration: %s
-                """, customFormat(duration1).replace(',', ' '),
-                     customFormat(duration2).replace(',', ' '));
+                        ArrayList  Duration: %s
+                        LinkedList Duration: %s
+                        ArrayList  Memory  : %s
+                        LinkedList Memory  : %s
+                        """, customFormat(duration1).replace(',', ' '),
+                customFormat(duration2).replace(',', ' '),
+                customFormat(used).replace(',', ' '),
+                customFormat(used0).replace(',', ' '));
+
+        System.gc();
     }
 
     static public String customFormat(long value) {

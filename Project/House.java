f8392445281a5f3f77d6a00964ecb4e2dcdc5c83
Yonchi03 <JoanDenchev@gmail.com>
2021-03-21 04:01:50 +0200

    Cleanup, official version

--- Project/House.java
+++ Project/House.java
@@ -1,17 +1,14 @@
 package Project;
 
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 
 class House extends Building {
 
-    //Fields
     private final int numFloors;
     private final String ownerName;
-    private static final List<House> houseList = new ArrayList<>();
+    private static final LinkedList<House> houseList = new LinkedList<>();
 
-    //Constructor
     protected House(float area, String address, int numFloors, String ownerName) {
         super(area, address);
         this.numFloors = numFloors;
@@ -19,12 +16,12 @@ class House extends Building {
         houseList.add(this);
     }
 
-    //Get method for the list
-    protected static List<House> getHouseList() { return houseList; }
+    protected static LinkedList<House> getHouseList() {
+        return houseList;
+    }
 
-    //Method to find the most spacious house
     private static House mostSpacious(){
-        House max = houseList.get(0);
+        House max = houseList.getFirst();
 
         for (House i : houseList) {
             if (i.getArea() / i.numFloors > max.getArea() / max.numFloors) max = i;
@@ -32,20 +29,17 @@ class House extends Building {
         return max;
     }
 
-    //Method for printing houses
     protected void fancyPrint(){
         System.out.printf("---------------------------------------------------%n");
         System.out.print(this);
         System.out.printf("---------------------------------------------------%n%n");
     }
 
-    //Method to print the most spacious house
     protected static void printMostSpacious(){
         System.out.println("НАЙ-ПРОСТОРНАТА КЪЩА Е:");
         mostSpacious().fancyPrint();
     }
 
-    //Override "toString" so the return value is as we want it
     @Override
     public String toString() {
         return  String.format("""
--- Project/Run2.java
+++ Project/Run2.java
@@ -4,29 +4,14 @@ public class Run2 {
 
     public static void main(String[] args) {
 
-        long before0 = Runtime.getRuntime().freeMemory();
-        long startTime2 = System.nanoTime();
-
         for (int i = 0; i < 10; i++) {
             new House(RandomNumbers.getRandomDouble(200, 700), "Location",
-                    RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"", true);
+                    RandomNumbers.getRandomInteger(1, 10), "\"The owner's name\"");
         }
 
-        for (House i : House.getHouseLinkedList()) i.fancyPrint();
-        House.printMostSpaciousLL();
+        for (House i : House.getHouseList()) i.fancyPrint();
+        House.printMostSpacious();
 
-        long endTime2 = System.nanoTime();
-        long used0 = before0 - Runtime.getRuntime().freeMemory();
-
-        long duration2 = (endTime2 - startTime2);
-
-        System.out.printf("""
-                            LinkedList Duration: %s
-                            LinkedList Memory  : %s
-                            """,
-                RandomNumbers.customFormat(duration2).replace(',', ' '),
-                RandomNumbers.customFormat(used0).replace(',', ' '));
-
-            System.gc();
-        }
+        System.gc();
+    }
 }
